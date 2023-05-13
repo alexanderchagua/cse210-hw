e10ce8ad2e1a29ca81c0f0a5e454fa694345ba95
alexander chagua <alexanderelton2887@gmail.com>
2023-05-13 03:45:01 -0500

    news

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Entry {
+    // in here i use the getter and setter
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+}
