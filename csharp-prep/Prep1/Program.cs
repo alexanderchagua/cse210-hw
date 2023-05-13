ef552cef8fe1d428891572050c127fefd760aa68
alexander chagua <alexanderelton2887@gmail.com>
2023-05-13 03:42:58 -0500

    news

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+        Console.WriteLine("what is your first name"); string name = Console.ReadLine();
+        Console.WriteLine("what is your first last name"); string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");}
+        
+        
+        }
+
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
-    }
-}
+  {
+        // Defina una lista de números vacía
+        List<int> numeros = new List<int>();
+
+        // Pida al usuario una serie de números y agregue cada uno a la lista
+        Console.WriteLine("Ingrese una serie de números. Ingrese 0 para terminar.");
+
+        int numero = -1;
+        while (numero != 0)
+        {
+            try
+            {
+                numero = Convert.ToInt32(Console.ReadLine());
+                numeros.Add(numero);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Por favor ingrese un número válido.");
+            }
+        }
+
+        // Calcule la suma de los números en la lista
+        int suma = numeros.Sum();
+
+        Console.WriteLine("La suma de los números en la lista es: " + suma);
+
+        // Calcule el promedio de los números en la lista
+        double promedio = numeros.Average();
+
+        Console.WriteLine("El promedio de los números en la lista es: " + promedio);
+
+        // Encuentre el número máximo o mayor en la lista
+        int maximo = numeros.Max();
+
+        Console.WriteLine("El número máximo en la lista es: " + maximo);
+
+        Console.ReadKey();
+    }}
